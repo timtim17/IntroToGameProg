0c48723293027728a25d6b4014fcf68b491f0a9c
Austin Jenchi <ajenchi9+code@gmail.com>
2015-01-27 19:42:18 -0800

    Finish Week 1

--- Homework/Week1.py
+++ Homework/Week1.py
@@ -12,3 +12,58 @@ print "   |   |   "
 print "-----------"
 print "   |   |   "
 print
+
+print "********** Exercise 1.4 Part II **********"
+print
+import math
+solution1 = (3 * 5)/(2+3)
+print str(solution1)
+print
+solution2 = math.sqrt(7 + 9) * 2
+print str(solution2)
+print
+solution3 = math.pow(4 - 7, 3)
+print str(solution3)
+print
+solution4 = (-19 + 100) ** (1/4)
+print str(solution4)
+solution5 = 6 % 4
+print str(solution5)
+print
+
+print "********** Exercise 1.4 Part III **********"
+problemA = (5 + 2)/3
+problemB = 5 + 2/3
+
+print "Problem A: ((5 + 2)/3) Solution: " + str(problemA)
+print "Problem B (5 + 2/3): Solution: " + str(problemB)
+print
+
+print "********** Exercise 1.5 **********"
+print
+firstName = raw_input("What is your first name? ==> ")
+lastName = raw_input("What is your last name? ==> ")
+
+print "Enter your date of birth:"
+while True:
+    try:
+        birthMonth = int(raw_input("    Month? ==> "))
+        if 0 < birthMonth < 13:
+            break
+    except ValueError: print "Nope, it has to be a number between 1 and 12."
+while True:
+    try:
+        birthDay = int(raw_input("    Day? ==> "))
+        if 0 < birthDay < 32:
+            break
+    except ValueError: print "Nope, it has to be a number between 1 and 31."
+while True:
+    try:
+        birthYear = raw_input("    Year? ==> ")
+        int(birthYear)  # Test to see if it actually is a number, not just four random things
+        if birthYear.length == 4:
+            break
+    except ValueError: print "Nope, it has to be a year (that's 4 numbers)."
+
+print firstName + lastName + " was born on " + birthMonth + "/" + birthDay + "/" + birthYear
+print
